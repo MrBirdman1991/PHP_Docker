d6dc1a3f0e6f87a8c05f00a4d09f3813140e999a
julian vogel <birdman-coding@outlook.de>
2022-06-27 18:04:01 +0200

    setup

---
+++ src/core/Application.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Core;
+
+class Application{
+    public function __construct()
+    {
+        echo "Peng!!!!";
+    }
+
+    public function run(){
+        echo "nice";
+    }
+}
